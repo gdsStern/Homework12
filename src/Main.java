41842535513295d7782bb39c029a546396c46ed2
User <git@github.com:gdsStern/HelloWorld.git>
2024-02-29 12:42:26 +0300

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,15 @@ public class Main {
         Author author1 = new Author("Федор","Достоевский");
         Author author2 = new Author("Николай","Гоголь");
         Book book1 = new Book("Преступление и наказание",author1,1866);
+        Book book3 = new Book("Преступление и наказание",author1,1866);
+        Book book4 = new Book("Преступление и наказание",author1,1865); //Другой год
         Book book2 = new Book("Мертвые души",author2,1842);
         book2.setYearPublication(1843);
+        System.out.println(author1.toString());
+        System.out.println(book1.toString());
+        System.out.println(book1.equals(book3));
+        System.out.println(book1.equals(book4));
+        System.out.println(book1.hashCode());
+        System.out.println(book3.hashCode());
     }
 }
